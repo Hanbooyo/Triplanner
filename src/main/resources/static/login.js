99923b056d5f805bb79b889fc3a25469369b068a
Hanbooyo <qnfudk@gmail.com>
2023-03-15 23:49:12 +0900

    프론트 페이지 sample

---
+++ src/main/resources/static/login.js
@@ -0,0 +1,11 @@
+// 구글 로그인 버튼 클릭 이벤트 핸들러
+const googleLoginButton = document.querySelector('.login-google-button');
+googleLoginButton.addEventListener('click', () => {
+  // 구글 로그인 로직 구현
+});
+
+// 페이스북 로그인 버튼 클릭 이벤트 핸들러
+const facebookLoginButton = document.querySelector('.login-facebook-button');
+facebookLoginButton.addEventListener('click', () => {
+  // 페이스북 로그인 로직 구현
+});
---
+++ src/main/resources/static/main.js
@@ -0,0 +1,7 @@
+// 탭 버튼 클릭 이벤트 핸들러
+const tabButtons = document.querySelectorAll('.main-tab-button');
+tabButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // 해당 탭으로 이동하는 로직 구현
+  });
+});
